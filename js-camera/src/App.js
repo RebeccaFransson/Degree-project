3ecaa7c075c38fa5f49858caf54aae630d60eb16
rebeccafransson <rf222cz@student.lnu.se>
2017-04-21 13:21:08 +0200

    Can open camera and take picture, not automatic

--- js-camera/src/App.js
+++ js-camera/src/App.js
@@ -1,21 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      showCamera: false,
+    }
+  }
+
+  activateCamera = () => {
+    console.log('starta kameran')
+    this.setState({
+      showCamera: true,
+    })
+  }
+
+  takePicture = () => {
+    console.log('tar bild')
+    var canvas = document.getElementById('canvas')
+    var context = canvas.getContext('2d')
+    
+    context.drawImage(document.getElementById('video'), 0, 0, 300, 150)
+    
+  }
+
+  componentDidUpdate(){
+    console.log('update')
+    var video = document.getElementById('video')
+    if(video != null){
+      const { takePicture } = this
+      if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+          navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+              video.src = window.URL.createObjectURL(stream)
+              video.play()
+              console.log('Öppnar video')
+              takePicture()
+          })
+      }
+    }
+  }
+
+
   render() {
+  const { showCamera } = this.state
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to the cameratest</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { showCamera ?
+          <div id='showing'>
+            <video id='video' autoPlay></video>
+            <button onClick={this.takePicture}> Snapshot </button>
+            <canvas id='canvas'></canvas>
+          </div>
+          :
+          <button onClick={this.activateCamera}> Activate Cameratest </button>
+        }
+        
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
